ae96fd96f6fcc9efdc4c5e47c88fc24bf9e09ba9
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-17 13:34:58 +1200

    configure server to use basicAuth middleware

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-	
+
 	// Create a new engine
 	engine := html.New("./views", ".html")
 
@@ -19,11 +19,23 @@ func main() {
 	})
 
 	app.Static("/public", "./public")
+	cfg := basicauth.Config{
+		Users: map[string]string{
+			"banana": "mango",
+		},
+	}
+
+	app.Use(basicauth.New(cfg))
+	// Group protected routes that require Basic Authentication
+	protected := app.Group("", basicauth.New(cfg))
+	protected.Get("/protected", func(c *fiber.Ctx) error {
+		return c.SendString("Protected route, authentication required!")
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		// Render index
 		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
+			"Title": "rcmndr",
 		}, "layouts/main")
 	})
 
