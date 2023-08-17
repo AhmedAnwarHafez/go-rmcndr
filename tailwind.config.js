d0bce83fb537b17737113917e2d81e509dab2d43
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-17 13:34:36 +1200

    add additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./views/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF17CE",
+        primaryLighter: "#FF80E4",
+        darkPurple: "#1B0636",
+        lightPurple: "#B07CF2",
+        extraLightPurple: "#E0C8FF",
+        warning: "#F27C7C",
+      },
+      fontFamily: {
+        serif: ['"Roboto Slab"', "serif"],
+        sans: ["Quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
