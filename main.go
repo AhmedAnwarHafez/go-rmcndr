26949eb2fe233b73eaa1f8f90c9801bd9c3d08a4
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-17 17:16:06 +1200

    refactor auth logic

--- main.go
+++ main.go
@@ -1,39 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 	"github.com/gofiber/template/html/v2"
 	"github.com/joho/godotenv"
+	"go-rcmndr/routes/auth"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/github"
 )
-
-type GitHubUser struct {
-	Login string `json:"login"`
-	ID    int    `json:"id"`
-}
-
-func getUserInfo(token *oauth2.Token, ctx *fiber.Ctx, config oauth2.Config) (*GitHubUser, error) {
-	client := config.Client(ctx.Context(), token)
-	resp, err := client.Get("https://api.github.com/user")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	user := &GitHubUser{}
-	err = json.NewDecoder(resp.Body).Decode(user)
-	if err != nil {
-		return nil, err
-	}
-
-	return user, nil
-}
 
 // session store
 var store = session.New()
@@ -44,11 +20,6 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	// Retrieve values
-	clientID := os.Getenv("GITHUB_CLIENT_ID")
-	clientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
-	callbackURL := os.Getenv("GITHUB_CALLBACK_URL")
-
 	// Create a new engine
 	engine := html.New("./views", ".html")
 
@@ -58,15 +29,6 @@ func main() {
 	})
 
 	app.Static("/public", "./public")
-	app.Use(printSessionMiddleware)
-
-	oauth2Config := oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		RedirectURL:  callbackURL,
-		Scopes:       []string{}, // Add required scopes here
-		Endpoint:     github.Endpoint,
-	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		// Render index
@@ -76,7 +38,8 @@ func main() {
 	})
 
 	app.Get("/login", func(c *fiber.Ctx) error {
-		authURL := oauth2Config.AuthCodeURL("", oauth2.AccessTypeOffline)
+		config := auth.GetConfig()
+		authURL := config.AuthCodeURL("", oauth2.AccessTypeOffline)
 		return c.Redirect(authURL)
 	})
 
@@ -93,9 +56,10 @@ func main() {
 
 	app.Get("/auth/github/callback", func(c *fiber.Ctx) error {
 		code := c.Query("code")
-		token, err := oauth2Config.Exchange(c.Context(), code)
+		config := auth.GetConfig()
+		token, err := config.Exchange(c.Context(), code)
 
-		user, err := getUserInfo(token, c, oauth2Config)
+		user, err := auth.GetUserInfo(token, c, config)
 		if err != nil {
 			return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 		}
@@ -132,20 +96,4 @@ func main() {
 	})
 
 	log.Fatal(app.Listen(":3000"))
-}
-
-func printSessionMiddleware(c *fiber.Ctx) error {
-	sess, err := store.Get(c)
-	if err != nil {
-		log.Println("Error retrieving session:", err)
-		return c.Next()
-	}
-
-	// Print the entire session store
-	log.Println("Session content:", sess.Fresh())
-
-	// Or print specific values
-	log.Println("User ID:", sess.Get("user_id"))
-
-	return c.Next()
 }
---
+++ routes/auth/auth.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/oauth2"
+)
+
+type GitHubUser struct {
+	Login string `json:"login"`
+	ID    int    `json:"id"`
+}
+
+func GetUserInfo(token *oauth2.Token, ctx *fiber.Ctx, config oauth2.Config) (*GitHubUser, error) {
+	client := config.Client(ctx.Context(), token)
+	resp, err := client.Get("https://api.github.com/user")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	user := &GitHubUser{}
+	err = json.NewDecoder(resp.Body).Decode(user)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func GetConfig() oauth2.Config {
+	// Retrieve values
+	clientID := os.Getenv("GITHUB_CLIENT_ID")
+	clientSecret := os.Getenv("GITHUB_CLIENT_SECRET")
+	callbackURL := os.Getenv("GITHUB_CALLBACK_URL")
+
+	oauth2Config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  callbackURL,
+		Scopes:       []string{}, // Add required scopes here
+		Endpoint:     github.Endpoint,
+	}
+
+	return oauth2Config
+}
