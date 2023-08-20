d08424cbe628b828f32e694537d96605694a7579
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-20 15:31:02 +1200

    setup search results

--- main.go
+++ main.go
@@ -50,8 +50,19 @@ func main() {
 	app.Get("/", HomeHandler)
 	app.Get("/search", func(c *fiber.Ctx) error {
 
+		q := c.Query("q", "default")
+
+		// means that htmx triggered this request
+		if c.Get("Hx-Trigger-Name") == "q" {
+			return c.Render("search-result", fiber.Map{
+				"Title": "rcmndr",
+				"Query": q,
+			}, "layouts/main")
+		}
+
 		return c.Render("search", fiber.Map{
 			"Title": "rcmndr",
+			"Query": q,
 		}, "layouts/main")
 	})
 
