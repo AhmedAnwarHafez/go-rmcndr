6837a903bda1976c4add6fbf6827c88b2e03a708
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-09-03 18:29:31 +1200

    display results

--- search.go
+++ search.go
@@ -12,20 +12,47 @@ func GetSearchHandler(c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-	rows, err := db.Query("SELECT * FROM users")
+	rows, err := db.Query(`SELECT u.nickname as Nickname, u.is_public as IsPublic, g.name as GenreName, COUNT(r.id) AS GenereCount
+FROM recommendations r
+JOIN users u ON r.user_id = u.id
+JOIN genres g ON r.genre_id = g.id
+WHERE u.nickname LIKE ?
+GROUP BY g.id`, "%br%")
+
 	if err != nil {
 		return err
 	}
 
-	users := []User{}
+	defer rows.Close()
+
+	type Genre struct {
+		GenreName string
+		Count     int64
+	}
+
+	type User struct {
+		Id       int64
+		Nickname string
+		IsPublic bool
+		Genres   []Genre
+	}
+
+	usersMap := make(map[string]*User)
+
 	for rows.Next() {
 		var user User
-		err = rows.Scan(&user.Id, &user.Nickname, &user.IsPublic)
+		var genre Genre
+		err := rows.Scan(&user.Nickname, &user.IsPublic, &genre.GenreName, &genre.Count)
 		if err != nil {
 			return err
 		}
+		// Check if user already exists in the map
+		if _, exists := usersMap[user.Nickname]; !exists {
+			usersMap[user.Nickname] = &User{Nickname: user.Nickname}
+		}
 
-		users = append(users, user)
+		// Append the genre to the user's genres
+		usersMap[user.Nickname].Genres = append(usersMap[user.Nickname].Genres, genre)
 	}
 
 	q := c.Query("q", "default")
@@ -42,6 +69,6 @@ func GetSearchHandler(c *fiber.Ctx) error {
 	return c.Render("search", fiber.Map{
 		"Title": "rcmndr",
 		"Query": q,
-		"Users": users,
+		"Users": usersMap,
 	}, "layouts/main")
 }
